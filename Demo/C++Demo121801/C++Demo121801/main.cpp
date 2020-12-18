771d1d002feb6d1a9b9c700e9a5acaff3a2acc72
Dulpyanghaobo <2533382191@qq.com>
2020-12-18 11:25:10 +0800

    构造函数

---
+++ Demo/C++Demo121801/C++Demo121801/main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  C++Demo121801
+//
+//  Created by yhb on 2020/12/18.
+//
+
+#include <iostream>
+using namespace std;
+
+
+//构造函数getter方法
+struct Person {
+    int m_age;
+    Person() {
+        cout << "Person()" << endl;
+    }
+    ~Person () {
+        cout << "endl()" << endl;
+    }
+    Person(int age) {
+        m_age = age;
+        cout << "PersonAGe()" << endl;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+//    Person p1(1);
+//    Person p2(2);
+//    Person p3(3);
+//
+    {
+        Person person;
+    }
+    Person *p4 = new Person(1);
+//    Person *p5 = (Person *)malloc(sizeof(Person));
+    delete p4;
+//    free(p5);
+    return 0;
+}
